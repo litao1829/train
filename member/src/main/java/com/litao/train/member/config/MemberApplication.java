96bd81678dc3e924fe046efbcfe402408c8063e1
litao <2532566074@qq.com>
2023-11-20 15:24:23 +0800

    新增member会员模块

---
+++ member/src/main/java/com/litao/train/member/config/MemberApplication.java
@@ -0,0 +1,14 @@
+package com.litao.train.member.config;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("com.litao")
+public class MemberApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MemberApplication.class,args);
+    }
+}
