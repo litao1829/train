a84731e93b84bc9ec56d0fdf4dc0a9a21fcfc546
litao <2532566074@qq.com>
2023-11-20 19:12:18 +0800

    使用自定义异常处理异常业务

--- common/src/main/java/com/litao/controller/ControllerExceptionHandler.java
+++ common/src/main/java/com/litao/controller/ControllerExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.litao.controller;
 
+import com.litao.exception.BusinessException;
 import com.litao.resp.CommonResp;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,8 +14,12 @@ public class ControllerExceptionHandler {
     private static final Logger LOG= LoggerFactory.getLogger(ControllerExceptionHandler.class);
 
 
-
-
+    /**
+     * 所有异常统一处理
+     * @param e
+     * @return
+     * @throws Exception
+     */
     @ExceptionHandler(value = Exception.class)
     @ResponseBody
     public CommonResp<?> exceptionHandler(Exception e) throws Exception{
@@ -24,5 +29,21 @@ public class ControllerExceptionHandler {
         commonResp.setSuccess(false);
         commonResp.setMessage(e.getMessage());
         return commonResp;
+    }
+
+
+    /**
+     * 业务异常统一处理
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(value = BusinessException.class)
+    @ResponseBody
+    public CommonResp<?> exceptionHandler(BusinessException e){
+        CommonResp<?> commonResp=new CommonResp<>();
+        LOG.error("业务异常：{}",e.getE().getDesc());
+        commonResp.setSuccess(false);
+        commonResp.setMessage(e.getE().getDesc());
+        return  commonResp;
     }
 }
--- member/src/main/java/com/litao/train/member/service/MemberService.java
+++ member/src/main/java/com/litao/train/member/service/MemberService.java
@@ -1,6 +1,8 @@
 package com.litao.train.member.service;
 
 import cn.hutool.core.collection.CollUtil;
+import com.litao.exception.BusinessException;
+import com.litao.exception.BusinessExceptionEnum;
 import com.litao.train.member.domain.Member;
 import com.litao.train.member.domain.MemberExample;
 import com.litao.train.member.mapper.MemberMapper;
@@ -25,7 +27,7 @@ public class MemberService {
         memberExample.createCriteria().andMobileEqualTo(mobile);
         List<Member> members = memberMapper.selectByExample(memberExample);
         if(CollUtil.isNotEmpty(members)){
-            throw  new RuntimeException("手机号已被注册");
+            throw  new BusinessException(BusinessExceptionEnum.MEMBER_MOBILE_EXIST);
         }
 
         Member member=new Member();
