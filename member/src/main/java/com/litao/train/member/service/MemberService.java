6e29a5bc0fd42270b7ce17efab5eedc04a87b4d9
litao <2532566074@qq.com>
2023-11-20 21:19:37 +0800

    短信验证码登录接口开发

--- member/src/main/java/com/litao/train/member/service/MemberService.java
+++ member/src/main/java/com/litao/train/member/service/MemberService.java
@@ -1,13 +1,17 @@
 package com.litao.train.member.service;
 
+import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.util.ObjectUtil;
 import com.litao.exception.BusinessException;
 import com.litao.exception.BusinessExceptionEnum;
 import com.litao.train.member.domain.Member;
 import com.litao.train.member.domain.MemberExample;
 import com.litao.train.member.mapper.MemberMapper;
+import com.litao.train.member.req.MemberLoginReq;
 import com.litao.train.member.req.MemberRegisterReq;
 import com.litao.train.member.req.MemberSendCodeReq;
+import com.litao.train.member.resp.MemberLoginResp;
 import com.litao.util.SnowUtil;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
@@ -21,20 +25,20 @@ public class MemberService {
     @Resource
     private MemberMapper memberMapper;
 
-    public int count(){
+    public int count() {
         return Math.toIntExact(memberMapper.countByExample(null));
     }
 
-    public long register(MemberRegisterReq req){
-        String mobile=req.getMobile();
-        MemberExample memberExample=new MemberExample();
+    public long register(MemberRegisterReq req) {
+        String mobile = req.getMobile();
+        MemberExample memberExample = new MemberExample();
         memberExample.createCriteria().andMobileEqualTo(mobile);
         List<Member> members = memberMapper.selectByExample(memberExample);
-        if(CollUtil.isNotEmpty(members)){
-            throw  new BusinessException(BusinessExceptionEnum.MEMBER_MOBILE_EXIST);
+        if (CollUtil.isNotEmpty(members)) {
+            throw new BusinessException(BusinessExceptionEnum.MEMBER_MOBILE_EXIST);
         }
 
-        Member member=new Member();
+        Member member = new Member();
         member.setId(SnowUtil.getSnowflakeNextId());
         member.setMobile(mobile);
         memberMapper.insert(member);
@@ -42,31 +46,61 @@ public class MemberService {
     }
 
 
-
-    public void sendCode(MemberSendCodeReq req){
-        String mobile= req.getMobile();
-        MemberExample memberExample=new MemberExample();
+    public void sendCode(MemberSendCodeReq req) {
+        String mobile = req.getMobile();
+        MemberExample memberExample = new MemberExample();
         memberExample.createCriteria().andMobileEqualTo(mobile);
         List<Member> list = memberMapper.selectByExample(memberExample);
 
         //如果手机号不存在，则插入一条记录
-        if(CollUtil.isEmpty(list)){
+        if (CollUtil.isEmpty(list)) {
             log.info("手机号不存在，插入一条记录");
-            Member member=new Member();
+            Member member = new Member();
             member.setId(SnowUtil.getSnowflakeNextId());
             member.setMobile(mobile);
             memberMapper.insert(member);
-        }else {
+        } else {
             log.info("手机号存在，不插入记录");
         }
 
         //生成验证码
-        String code="8888";
+        String code = "8888";
 
-        log.info("生成短信验证码：{}",code);
+        log.info("生成短信验证码：{}", code);
         //保存短信记录表：手机号，短信验证码，有效期，是否已使用，业务类型，发送时间，使用时间
         log.info("保存短信记录表");
         //对接短信通道，发送短信
         log.info("对接短信通道");
+    }
+
+    public MemberLoginResp login(MemberLoginReq req) {
+        String mobile = req.getMobile();
+        String code = req.getCode();
+        Member memberDB = selectByMobile(mobile);
+        //如果手机号不存在，则插入一条记录
+        if (ObjectUtil.isNull(memberDB)) {
+            throw new BusinessException(BusinessExceptionEnum.MEMBER_MOBILE_NOT_EXIST);
+        }
+
+        //校验短信验证码
+        if (!("8888".equals(code))) {
+            throw new BusinessException(BusinessExceptionEnum.MEMBER_MOBILE_CODE_ERROR);
+        }
+        return BeanUtil.copyProperties(memberDB, MemberLoginResp.class);
+    }
+
+
+    public Member selectByMobile(String mobile){
+        MemberExample memberExample=new MemberExample();
+
+        memberExample.createCriteria().andMobileEqualTo(mobile);
+        List<Member> members = memberMapper.selectByExample(memberExample);
+        System.out.println(members);
+        if(CollUtil.isEmpty(members)){
+            return null;
+        }
+        else {
+            return members.get(0);
+        }
     }
 }
