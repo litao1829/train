4c3acf34d4422dc0818550b21e51cbeb49cbaba4
litao <2532566074@qq.com>
2023-11-21 20:59:09 +0800

    集成 DOM4j 读取 xml

--- geneator/src/main/java/com/litao/train/generator/gen/ServerGenerator.java
+++ geneator/src/main/java/com/litao/train/generator/gen/ServerGenerator.java
@@ -1,6 +1,10 @@
 package com.litao.train.generator.gen;
 
 import freemarker.template.TemplateException;
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Node;
+import org.dom4j.io.SAXReader;
 import com.litao.train.generator.util.FreemarkerUtil;
 
 import java.io.File;
@@ -15,15 +19,34 @@ import java.util.Map;
  **/
 public class ServerGenerator {
     static String toPath = "geneator\\src\\main\\java\\com\\litao\\train\\generator\\test\\";
-
+    static String pomPath = "geneator\\pom.xml";
     static {
         new File(toPath).mkdirs();
     }
 
-    public static void main(String[] args) throws IOException, TemplateException {
-        FreemarkerUtil.initConfig("test.ftl");
-        Map<String, Object> param = new HashMap<>();
-        param.put("domain", "Test1");
-        FreemarkerUtil.generator(toPath + "Test1.java", param);
+    public static void main(String[] args) throws Exception {
+        String generatorPath = getGeneratorPath();
+
+        Document document = new SAXReader().read("geneator/" + generatorPath);
+        Node table = document.selectSingleNode("//table");
+        System.out.println(table);
+        Node tableName = table.selectSingleNode("@tableName");
+        Node domainObjectName = table.selectSingleNode("@domainObjectName");
+        System.out.println(tableName.getText() + "/" + domainObjectName.getText());
+
+//        FreemarkerUtil.initConfig("test.ftl");
+//        Map<String, Object> param = new HashMap<>();
+//        param.put("domain", "Test1");
+//        FreemarkerUtil.generator(toPath + "Test1.java", param);
+    }
+    private static String getGeneratorPath() throws DocumentException {
+        SAXReader saxReader = new SAXReader();
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("pom", "http://maven.apache.org/POM/4.0.0");
+        saxReader.getDocumentFactory().setXPathNamespaceURIs(map);
+        Document document = saxReader.read(pomPath);
+        Node node = document.selectSingleNode("//pom:configurationFile");
+        System.out.println(node.getText());
+        return node.getText();
     }
 }
