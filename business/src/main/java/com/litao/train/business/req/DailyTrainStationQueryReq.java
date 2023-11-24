82ab5dd751e042424133c24a1baad5be768282b0
litao <2532566074@qq.com>
2023-11-24 09:07:17 +0800

    完善每日车站管理页面：增加日期车次查询条件、站名拼音、停站时间自动计算

--- business/src/main/java/com/litao/train/business/req/DailyTrainStationQueryReq.java
+++ business/src/main/java/com/litao/train/business/req/DailyTrainStationQueryReq.java
@@ -1,11 +1,41 @@
 package com.litao.train.business.req;
 
 import com.litao.req.PageReq;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
 
 public class DailyTrainStationQueryReq extends PageReq {
-@Override
-public String toString() {
-return "DailyTrainStationQueryReq{" +
-"} " + super.toString();
-}
+
+    /**
+     * 车次编号
+     */
+    private String code;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date date;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "DailyTrainStationQueryReq{" +
+                "code='" + code + '\'' +
+                ", date=" + date +
+                '}';
+    }
 }
