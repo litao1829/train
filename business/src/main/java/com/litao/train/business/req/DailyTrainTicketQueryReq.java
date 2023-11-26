45702df22bdea8eae17e40c065a18ed40d9bcd6b
litao <2532566074@qq.com>
2023-11-26 19:34:50 +0800

    为余票信息页面增加查询条件

--- business/src/main/java/com/litao/train/business/req/DailyTrainTicketQueryReq.java
+++ business/src/main/java/com/litao/train/business/req/DailyTrainTicketQueryReq.java
@@ -1,11 +1,44 @@
 package com.litao.train.business.req;
 
 import com.litao.req.PageReq;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import java.util.Date;
+
+@Getter
+@Setter
 public class DailyTrainTicketQueryReq extends PageReq {
-@Override
-public String toString() {
-return "DailyTrainTicketQueryReq{" +
-"} " + super.toString();
-}
+
+    /**
+     * 日期
+     */
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date date;
+
+    /**
+     * 车次编号
+     */
+    private  String trainCode;
+
+    /**
+     * 出发站
+     */
+    private String start;
+
+    /**
+     * 到达站
+     */
+    private String end;
+
+    @Override
+    public String toString() {
+        return "DailyTrainTicketQueryReq{" +
+                "date=" + date +
+                ", trainCode='" + trainCode + '\'' +
+                ", start='" + start + '\'' +
+                ", end='" + end + '\'' +
+                '}';
+    }
 }
